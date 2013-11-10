a344f5dbf2cd9fe6dec060958146e84d225d551b
Mikkel Malmberg <mikkelmalmberg@me.com>
2013-11-10 22:07:11 +0100

    fix indentation

--- lib/foundation-will_paginate/helper.rb
+++ lib/foundation-will_paginate/helper.rb
@@ -1,16 +1,16 @@
 module Foundation
   module WillPaginate
     module Helper
-	    def foundation_paginate(pages, opts = {})
-	      defaults = {
-	        :class => 'pagination',
-	        :inner_window => 2,
-	        :outer_window => 0,
-	        :renderer => Foundation::WillPaginate::Renderer,
-	        :previous_label => '&laquo;'.html_safe,
-	        :next_label => '&raquo;'.html_safe }
-	      will_paginate(pages, defaults.merge(opts))
-	    end
-	  end
-	end
+      def foundation_paginate(pages, opts = {})
+        defaults = {
+          :class => 'pagination',
+          :inner_window => 2,
+          :outer_window => 0,
+          :renderer => Foundation::WillPaginate::Renderer,
+          :previous_label => '&laquo;'.html_safe,
+          :next_label => '&raquo;'.html_safe }
+        will_paginate(pages, defaults.merge(opts))
+      end
+    end
+  end
 end
